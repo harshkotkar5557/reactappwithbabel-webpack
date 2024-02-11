2d561b414ddf710166fd34f0db1e8784924a5114
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2024-02-11 13:45:06 +0000

    basic-project

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import * as React from "react"
+import "./style.css"
+
+export default function App () {
+    return <div className="app" style={{backgroundColor: "red"}}>Hello Haresh!, React is now here</div>
+}
