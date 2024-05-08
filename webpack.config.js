e91fe46679868a041dd3a8e5b28cd685f17884bc
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2024-05-08 13:36:24 +0000

    fix the issue

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    filename: 'index.js',
   },
   target: 'web',
   devServer: {
